12b4c2f7019ab4c09f59cf6157449665cf150971
Madhavmenon2022 <163705814+Madhavmenon2022@users.noreply.github.com>
2026-06-05 15:36:56 +0530

    Create 45-JumpGameIi

---
+++ Solutions/45-JumpGameIi/45-JumpGameIi.py
@@ -0,0 +1,16 @@
+# Last updated: 6/5/2026, 3:36:52 PM
+class Solution(object):
+    # @param A, a list of integers
+    # @return an integer
+    def jump(self, A):
+        jump_count = 0
+        reachable = 0
+        curr_reachable = 0
+        for i, length in enumerate(A):
+            if i > reachable:
+                return -1
+            if i > curr_reachable:
+                curr_reachable = reachable
+                jump_count += 1
+            reachable = max(reachable, i + length)
+        return jump_count
