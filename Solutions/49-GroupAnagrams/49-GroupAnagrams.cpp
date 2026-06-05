97178dd0b5d41766521bb6f1fd4f8bd636c40f5c
Madhavmenon2022 <163705814+Madhavmenon2022@users.noreply.github.com>
2026-06-05 16:05:36 +0530

    File updated at 6/5/2026, 4:05:32 PM

--- Solutions/49-GroupAnagrams/49-GroupAnagrams.cpp
+++ Solutions/49-GroupAnagrams/49-GroupAnagrams.cpp
@@ -1,16 +1,24 @@
-// Last updated: 6/5/2026, 4:03:59 PM
+// Last updated: 6/5/2026, 4:05:32 PM
 1class Solution {
 2public:
-3    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-4        sort(begin(intervals), end(intervals));
+3    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+4        size_t i = 0;
 5        vector<vector<int>> result;
-6        for (const auto& interval : intervals) {
-7            if (empty(result) || interval[0] > result.back()[1]) { 
-8                result.emplace_back(interval);
-9            } else {
-10                result.back()[1] = max(result.back()[1], interval[1]);
-11            }
-12        }
-13        return result;
-14    }
-15};
+6        // Insert intervals appeared before newInterval.
+7        while (i < size(intervals) && newInterval[0] > intervals[i][1]) {
+8            result.emplace_back(intervals[i++]);
+9        }
+10
+11        // Merge intervals that overlap with newInterval.
+12        while (i < size(intervals) && newInterval[1] >= intervals[i][0]) {
+13            newInterval = {min(newInterval[0], intervals[i][0]),
+14                           max(newInterval[1], intervals[i][1])};
+15            ++i;
+16        }
+17        result.emplace_back(newInterval);
+18
+19        // Insert intervals appearing after newInterval.
+20        copy(cbegin(intervals) + i, cend(intervals), back_inserter(result));
+21        return result;
+22    }
+23};
