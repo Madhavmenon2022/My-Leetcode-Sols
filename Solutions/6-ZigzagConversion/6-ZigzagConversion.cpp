2facef44859f76693a18b64f2d2c737c3029df29
Madhavmenon2022 <163705814+Madhavmenon2022@users.noreply.github.com>
2026-06-05 15:37:49 +0530

    Create 6-ZigzagConversion

---
+++ Solutions/6-ZigzagConversion/6-ZigzagConversion.cpp
@@ -0,0 +1,21 @@
+// Last updated: 6/5/2026, 3:37:45 PM
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+        const int step = 2 * numRows - 2;
+        string zigzag;
+        for (int i = 0; i < numRows; ++i) {
+            for (int j = i; j < s.length(); j += step) {
+                zigzag.push_back(s[j]);
+                if (0 < i && i < numRows - 1 &&
+                    j + step - 2 * i < s.length()) {
+                    zigzag.push_back(s[j + step - 2 * i]);
+                }
+            }
+        }
+        return zigzag;
+    }
+};
