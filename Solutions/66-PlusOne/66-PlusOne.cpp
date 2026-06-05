b72f5b112ebf9b64a914d236f09eced7729b07b8
Madhavmenon2022 <163705814+Madhavmenon2022@users.noreply.github.com>
2026-06-05 20:41:39 +0530

    File updated at 6/5/2026, 8:41:35 PM

--- Solutions/66-PlusOne/66-PlusOne.cpp
+++ Solutions/66-PlusOne/66-PlusOne.cpp
@@ -1,21 +1,13 @@
-// Last updated: 6/5/2026, 8:39:58 PM
+// Last updated: 6/5/2026, 8:41:35 PM
 1class Solution {
 2public:
-3    int mySqrt(int x) {
-4        if (x < 2) {
-5            return x;
-6        }
-7        
-8        int left = 1, right = x / 2;
-9        while (left <= right) {
-10            const auto mid = left + (right - left) / 2;
-11            if (mid > x / mid) {
-12                right = mid - 1;
-13            } else {
-14                left = mid + 1;
-15            }
-16        }
-17
-18        return left - 1;
-19    }
-20};
+3    int climbStairs(int n) {
+4        vector<int> steps(3, 0);
+5        steps[0] = 1;
+6        steps[1] = 1;
+7        for (int i = 2; i <= n; ++i) {
+8            steps[i % 3] = steps[(i - 1) % 3] + steps[(i - 2) % 3];
+9        }
+10        return steps[n % 3];
+11    }
+12};
