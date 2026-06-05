8df4d4f0c7c8391b04a4a35adfd93d4c07686fd8
Madhavmenon2022 <163705814+Madhavmenon2022@users.noreply.github.com>
2026-06-05 16:01:50 +0530

    Create 52-NQueensIi

---
+++ Solutions/52-NQueensIi/52-NQueensIi.py
@@ -0,0 +1,22 @@
+# Last updated: 6/5/2026, 4:01:46 PM
+class Solution(object):
+    def totalNQueens(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        def dfs(row):
+            if row == n:
+                return 1
+            result = 0
+            for i in xrange(n):
+                if cols[i] or main_diag[row+i] or anti_diag[row-i+(n-1)]:
+                    continue
+                cols[i] = main_diag[row+i] = anti_diag[row-i+(n-1)] = True
+                result += dfs(row+1)
+                cols[i] = main_diag[row+i] = anti_diag[row-i+(n-1)] = False
+            return result
+
+        result = []
+        cols, main_diag, anti_diag = [False]*n, [False]*(2*n-1), [False]*(2*n-1)
+        return dfs(0)
