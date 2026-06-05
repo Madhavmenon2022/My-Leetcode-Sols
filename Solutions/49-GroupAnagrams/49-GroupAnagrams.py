5ec9e2ec388feaac13ce7d797232f6776842910c
Madhavmenon2022 <163705814+Madhavmenon2022@users.noreply.github.com>
2026-06-05 15:55:17 +0530

    File updated at 6/5/2026, 3:55:14 PM

--- Solutions/49-GroupAnagrams/49-GroupAnagrams.py
+++ Solutions/49-GroupAnagrams/49-GroupAnagrams.py
@@ -1,24 +1,22 @@
-# Last updated: 6/5/2026, 3:54:33 PM
+# Last updated: 6/5/2026, 3:55:14 PM
 1class Solution(object):
-2    def solveNQueens(self, n):
+2    def totalNQueens(self, n):
 3        """
 4        :type n: int
-5        :rtype: List[List[str]]
+5        :rtype: int
 6        """
 7        def dfs(row):
 8            if row == n:
-9                result.append(map(lambda x: '.'*x + "Q" + '.'*(n-x-1), curr))
-10                return
+9                return 1
+10            result = 0
 11            for i in xrange(n):
 12                if cols[i] or main_diag[row+i] or anti_diag[row-i+(n-1)]:
 13                    continue
 14                cols[i] = main_diag[row+i] = anti_diag[row-i+(n-1)] = True
-15                curr.append(i)
-16                dfs(row+1)
-17                curr.pop()
-18                cols[i] = main_diag[row+i] = anti_diag[row-i+(n-1)] = False
-19
-20        result, curr = [], []
-21        cols, main_diag, anti_diag = [False]*n, [False]*(2*n-1), [False]*(2*n-1)
-22        dfs(0)
-23        return result
+15                result += dfs(row+1)
+16                cols[i] = main_diag[row+i] = anti_diag[row-i+(n-1)] = False
+17            return result
+18
+19        result = []
+20        cols, main_diag, anti_diag = [False]*n, [False]*(2*n-1), [False]*(2*n-1)
+21        return dfs(0)
