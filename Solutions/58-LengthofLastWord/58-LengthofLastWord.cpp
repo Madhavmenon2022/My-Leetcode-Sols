bb0ac8c602e395246ab8d2c05de762b8655858b5
Madhavmenon2022 <163705814+Madhavmenon2022@users.noreply.github.com>
2026-06-05 18:13:56 +0530

    File updated at 6/5/2026, 6:13:52 PM

--- Solutions/58-LengthofLastWord/58-LengthofLastWord.cpp
+++ Solutions/58-LengthofLastWord/58-LengthofLastWord.cpp
@@ -1,31 +1,36 @@
-// Last updated: 6/5/2026, 6:12:52 PM
-1class Solution {
-2public:
-3    string getPermutation(int n, int k) {
-4        vector<int> nums;
-5        int total = 1;
-6        for (int i = 1; i <= n; ++i) {
-7            nums.emplace_back(i);
-8            total *= i;
-9        }
-10
-11        // Cantor Ordering:
-12        // Construct the k-th permutation with a list of n numbers
-13        // Idea: group all permutations according to their first number (so n groups, each of
-14        // (n - 1)! numbers), find the group where the k-th permutation belongs, remove the common
-15        // first number from the list and append it to the resulting string, and iteratively
-16        // construct the (((k - 1) % (n - 1)!) + 1)-th permutation with the remaining n-1 numbers
-17        int group = total;
-18        stringstream permutation;
-19        while (n > 0) {
-20            group /= n;
-21            int idx = (k - 1) / group;
-22            permutation << nums[idx];
-23            nums.erase(nums.begin() + idx);
-24            k = (k - 1) % group + 1;
-25            --n;
-26        }
-27
-28        return permutation.str();
-29    }
-30};
+// Last updated: 6/5/2026, 6:13:52 PM
+1/**
+2 * Definition for singly-linked list.
+3 * struct ListNode {
+4 *     int val;
+5 *     ListNode *next;
+6 *     ListNode() : val(0), next(nullptr) {}
+7 *     ListNode(int x) : val(x), next(nullptr) {}
+8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+9 * };
+10 */
+11class Solution {
+12public:
+13    ListNode* rotateRight(ListNode* head, int k) {
+14        if (head == nullptr || head->next == nullptr) {
+15            return head;
+16        }
+17
+18        int n = 1;
+19        auto curr = head;
+20        for (; curr->next; curr = curr->next) {
+21            ++n;
+22        }
+23        curr->next = head;
+24
+25        auto tail = curr;
+26        k = n - k % n;
+27        curr = head;
+28        for (int i = 0; i < k; curr = curr->next, ++i) {
+29            tail = curr;
+30        }
+31
+32        tail->next = nullptr;
+33        return curr; 
+34    }
+35};
