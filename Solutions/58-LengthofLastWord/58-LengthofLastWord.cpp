1c585a756ffd42fa23fa7c656773f1000fc926bb
Madhavmenon2022 <163705814+Madhavmenon2022@users.noreply.github.com>
2026-06-05 18:14:33 +0530

    File updated at 6/5/2026, 6:14:29 PM

--- Solutions/58-LengthofLastWord/58-LengthofLastWord.cpp
+++ Solutions/58-LengthofLastWord/58-LengthofLastWord.cpp
@@ -1,36 +1,14 @@
-// Last updated: 6/5/2026, 6:13:52 PM
-1/**
-2 * Definition for singly-linked list.
-3 * struct ListNode {
-4 *     int val;
-5 *     ListNode *next;
-6 *     ListNode() : val(0), next(nullptr) {}
-7 *     ListNode(int x) : val(x), next(nullptr) {}
-8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
-9 * };
-10 */
-11class Solution {
-12public:
-13    ListNode* rotateRight(ListNode* head, int k) {
-14        if (head == nullptr || head->next == nullptr) {
-15            return head;
-16        }
-17
-18        int n = 1;
-19        auto curr = head;
-20        for (; curr->next; curr = curr->next) {
-21            ++n;
-22        }
-23        curr->next = head;
-24
-25        auto tail = curr;
-26        k = n - k % n;
-27        curr = head;
-28        for (int i = 0; i < k; curr = curr->next, ++i) {
-29            tail = curr;
-30        }
-31
-32        tail->next = nullptr;
-33        return curr; 
-34    }
-35};
+// Last updated: 6/5/2026, 6:14:29 PM
+1class Solution {
+2    public:
+3        unordered_map<int, unordered_map<int, int> > hash;
+4        int uniquePaths(int m, int n) {
+5            if(m == 0 || n == 0)
+6                return 0;
+7            if(m == 1 || n == 1)
+8                return 1;
+9            if(hash.find(m) != hash.end() && hash[m].find(n) != hash[m].end())
+10                return hash[m][n];
+11            return hash[m][n] = uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
+12        }
+13};
