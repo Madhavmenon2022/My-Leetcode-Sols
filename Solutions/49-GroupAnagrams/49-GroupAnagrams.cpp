ed3c7a1bb78e7090e1b88fa1d6df849098fc44fc
Madhavmenon2022 <163705814+Madhavmenon2022@users.noreply.github.com>
2026-06-05 16:03:49 +0530

    File updated at 6/5/2026, 4:03:45 PM

--- Solutions/49-GroupAnagrams/49-GroupAnagrams.cpp
+++ Solutions/49-GroupAnagrams/49-GroupAnagrams.cpp
@@ -1,11 +1,16 @@
-// Last updated: 6/5/2026, 4:00:30 PM
+// Last updated: 6/5/2026, 4:03:45 PM
 1class Solution {
 2public:
-3    bool canJump(vector<int>& nums) {
-4        int reachable = 0;
-5        for (int i = 0; i <= reachable && i < nums.size(); ++i) {
-6            reachable = max(reachable, i + nums[i]);
-7        }
-8        return reachable >= nums.size() - 1;
-9    }
-10};
+3    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+4        sort(begin(intervals), end(intervals));
+5        vector<vector<int>> result;
+6        for (const auto& interval : intervals) {
+7            if (empty(result) || interval[0] > result.back()[1]) { 
+8                result.emplace_back(interval);
+9            } else {
+10                result.back()[1] = max(result.back()[1], interval[1]);
+11            }
+12        }
+13        return result;
+14    }
+15};
