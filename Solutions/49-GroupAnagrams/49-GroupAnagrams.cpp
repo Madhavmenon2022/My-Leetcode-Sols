a4a0090d4e5ca3929dce0ed29b6c2813810954b8
Madhavmenon2022 <163705814+Madhavmenon2022@users.noreply.github.com>
2026-06-05 16:00:34 +0530

    File updated at 6/5/2026, 4:00:30 PM

--- Solutions/49-GroupAnagrams/49-GroupAnagrams.cpp
+++ Solutions/49-GroupAnagrams/49-GroupAnagrams.cpp
@@ -1,31 +1,11 @@
-// Last updated: 6/5/2026, 3:58:15 PM
+// Last updated: 6/5/2026, 4:00:30 PM
 1class Solution {
 2public:
-3    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-4        vector<int> res;
-5        if (matrix.empty()) {
-6            return res;
+3    bool canJump(vector<int>& nums) {
+4        int reachable = 0;
+5        for (int i = 0; i <= reachable && i < nums.size(); ++i) {
+6            reachable = max(reachable, i + nums[i]);
 7        }
-8
-9        for (int left = 0, right = matrix[0].size() - 1,
-10                 top = 0, bottom = matrix.size() - 1;
-11             left <= right && top <= bottom;
-12             ++left, --right, ++top, --bottom) {
-13
-14            for (int j = left; j <= right; ++j) {
-15                res.emplace_back(matrix[top][j]);
-16            }
-17            for (int i = top + 1; i < bottom; ++i) {
-18                res.emplace_back(matrix[i][right]);
-19            }
-20            for (int j = right; top < bottom && j >= left; --j) {
-21                res.emplace_back(matrix[bottom][j]);
-22            }
-23            for (int i = bottom - 1; left < right && i > top; --i) {
-24                res.emplace_back(matrix[i][left]);
-25            }
-26        }
-27
-28        return res;
-29    }
-30};
+8        return reachable >= nums.size() - 1;
+9    }
+10};
