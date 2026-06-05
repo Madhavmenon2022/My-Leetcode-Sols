e19960c78d9dec147c8ad457c94921858be546e1
Madhavmenon2022 <163705814+Madhavmenon2022@users.noreply.github.com>
2026-06-05 16:01:52 +0530

    Create 50-PowxN

---
+++ Solutions/50-PowxN/50-PowxN.cpp
@@ -0,0 +1,16 @@
+// Last updated: 6/5/2026, 4:01:49 PM
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double result = 1;
+        long long abs_n = abs(static_cast<long long>(n));
+        while (abs_n > 0) {
+            if (abs_n & 1) {
+                result *= x;
+            }
+            abs_n >>= 1;
+            x *= x;
+        }
+        return n < 0 ? 1 / result : result;
+    }
+};
