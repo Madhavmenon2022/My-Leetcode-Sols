22886c13c2549bd4811fd75e3ea6ed2cd4c39499
Madhavmenon2022 <163705814+Madhavmenon2022@users.noreply.github.com>
2026-06-05 18:17:13 +0530

    File updated at 6/5/2026, 6:17:09 PM

--- Solutions/58-LengthofLastWord/58-LengthofLastWord.py
+++ Solutions/58-LengthofLastWord/58-LengthofLastWord.py
@@ -1,22 +1,15 @@
-# Last updated: 6/5/2026, 6:16:24 PM
+# Last updated: 6/5/2026, 6:17:09 PM
 1class Solution(object):
-2    # @param obstacleGrid, a list of lists of integers
+2    # @param grid, a list of lists of integers
 3    # @return an integer
-4    def uniquePathsWithObstacles(self, obstacleGrid):
-5        """
-6        :type obstacleGrid: List[List[int]]
-7        :rtype: int
-8        """
-9        m, n = len(obstacleGrid), len(obstacleGrid[0])
-10
-11        ways = [0]*n
-12        ways[0] = 1
-13        for i in xrange(m):
-14            if obstacleGrid[i][0] == 1:
-15                ways[0] = 0
-16            for j in xrange(n):
-17                if obstacleGrid[i][j] == 1:
-18                    ways[j] = 0
-19                elif j>0:
-20                    ways[j] += ways[j-1]
-21        return ways[-1]
+4    def minPathSum(self, grid):
+5        sum = list(grid[0])
+6        for j in xrange(1, len(grid[0])):
+7            sum[j] = sum[j - 1] + grid[0][j]
+8
+9        for i in xrange(1, len(grid)):
+10            sum[0] += grid[i][0]
+11            for j in xrange(1, len(grid[0])):
+12                sum[j] = min(sum[j - 1], sum[j]) + grid[i][j]
+13
+14        return sum[-1]
