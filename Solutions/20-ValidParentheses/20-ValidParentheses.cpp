c68bfd7f5e34b4ebbb54906600bee2eac26660d3
Madhavmenon2022 <163705814+Madhavmenon2022@users.noreply.github.com>
2026-06-05 15:37:28 +0530

    Create 20-ValidParentheses

---
+++ Solutions/20-ValidParentheses/20-ValidParentheses.cpp
@@ -0,0 +1,23 @@
+// Last updated: 6/5/2026, 3:37:24 PM
+class Solution {
+public:
+    bool isValid(string s) {
+        const  unordered_map<char, char> symbol_pair = {{')', '('},
+                                                        {']', '['},
+                                                        {'}', '{'}};
+        stack<char> parentheses;
+        for (const auto& c: s) {
+            const auto& it = symbol_pair.find(c);
+            if (it != symbol_pair.cend()) {
+                if (parentheses.empty() ||
+                    parentheses.top() != it->second) {
+                    return false;
+                }
+                parentheses.pop();
+            } else {
+                parentheses.emplace(c);
+            }
+        }
+        return parentheses.empty();
+    }
+};
