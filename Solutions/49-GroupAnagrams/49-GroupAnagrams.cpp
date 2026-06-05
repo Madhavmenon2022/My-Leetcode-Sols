6b455c2d09bf42a51b8962875449a6921f4d71c8
Madhavmenon2022 <163705814+Madhavmenon2022@users.noreply.github.com>
2026-06-05 15:53:08 +0530

    File updated at 6/5/2026, 3:53:04 PM

--- Solutions/49-GroupAnagrams/49-GroupAnagrams.cpp
+++ Solutions/49-GroupAnagrams/49-GroupAnagrams.cpp
@@ -1,24 +1,16 @@
-// Last updated: 6/5/2026, 3:51:50 PM
+// Last updated: 6/5/2026, 3:53:04 PM
 1class Solution {
 2public:
-3    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-4        unordered_map<string, vector<string>> groups;
-5        for (const auto& str : strs) {
-6            string tmp{str};
-7            sort(tmp.begin(), tmp.end());
-8            groups[tmp].emplace_back(str);
-9        }
-10
-11        vector<vector<string>> anagrams;
-12        for (const auto& kvp : groups) {
-13            vector<string> group;
-14            for (const auto& str : kvp.second) {
-15                group.emplace_back(str);
-16            }
-17            sort(group.begin(), group.end());
-18            anagrams.emplace_back(move(group));
-19        }
-20
-21        return anagrams;
-22    }
-23};
+3    double myPow(double x, int n) {
+4        double result = 1;
+5        long long abs_n = abs(static_cast<long long>(n));
+6        while (abs_n > 0) {
+7            if (abs_n & 1) {
+8                result *= x;
+9            }
+10            abs_n >>= 1;
+11            x *= x;
+12        }
+13        return n < 0 ? 1 / result : result;
+14    }
+15};
