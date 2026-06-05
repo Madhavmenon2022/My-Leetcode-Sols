19c74d503897ac9a2bba0a36198de3e16c1b4fab
Madhavmenon2022 <163705814+Madhavmenon2022@users.noreply.github.com>
2026-06-05 18:19:34 +0530

    Create 64-MinimumPathSum

---
+++ Solutions/64-MinimumPathSum/64-MinimumPathSum.py
@@ -0,0 +1,15 @@
+# Last updated: 6/5/2026, 6:19:30 PM
+class Solution(object):
+    # @param grid, a list of lists of integers
+    # @return an integer
+    def minPathSum(self, grid):
+        sum = list(grid[0])
+        for j in xrange(1, len(grid[0])):
+            sum[j] = sum[j - 1] + grid[0][j]
+
+        for i in xrange(1, len(grid)):
+            sum[0] += grid[i][0]
+            for j in xrange(1, len(grid[0])):
+                sum[j] = min(sum[j - 1], sum[j]) + grid[i][j]
+
+        return sum[-1]
